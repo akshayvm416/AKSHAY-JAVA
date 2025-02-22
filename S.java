c3c9a97402395040e2d5c8514a37876604b813d4
akshayvm416 <akshayvm416@gmail.com>
2025-02-23 00:02:27 +0530

    switch operation

---
+++ S.java
@@ -0,0 +1,15 @@
+public class S {
+    public static void main(String args[])
+    {
+        int n = 1;
+        switch(n)
+        {
+            case 1:
+            System.out.println("monday");
+            break;
+            case 2:
+            System.out.println("tuesday");
+            break;
+        }
+    }
+}
