a7dd46ddae2f2fff75ffb44ff0ab726a191f8ed3
akshayvm416 <akshayvm416@gmail.com>
2025-02-23 12:17:35 +0530

    nested while loop

---
+++ loop.java
@@ -0,0 +1,20 @@
+public class loop {
+    public static void main(String args[])
+    {
+         int i = 1;
+
+       while(i<=5)
+       {
+        System.out.println("hi" + i);
+        int j = 1;
+        while(j<=3){
+            System.out.println("hello" + j);
+            j++;
+        }
+        i++;
+
+       }
+
+    }
+    
+}
