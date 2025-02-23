f86b4083420f7282af191cdefead2ab9cfff1a2c
akshayvm416 <akshayvm416@gmail.com>
2025-02-23 12:32:13 +0530

    do while loop

---
+++ dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 4;
+        do
+        {
+            System.out.println("hi" + i);
+            i++;
+        }while(i<=4);
+    }
+    
+}
