975167209302ee591cf70228c6e177a11dc73121
akshayvm416 <akshayvm416@gmail.com>
2025-02-26 23:41:52 +0530

    stack

---
+++ st.java
@@ -0,0 +1,38 @@
+class Calculator 
+{
+    int num = 5;
+    public int add(int n1, int n2)
+    {
+        return n1 + n2 ;
+    }
+}
+public class st  {
+    public static void main(String a[])
+    {
+        int data =10;
+
+        Computer obj = new Computer();
+        Calculator obj1 = new Calculator();
+        int r1 = obj.add (3,4);
+        System.out.println(r1);
+        System.out.println();
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class st {
+    
+}
